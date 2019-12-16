5e99ea7041761f786ec2fea06abf489f6cac7657
Rong Ying <kohrongying@gmail.com>
2019-12-16 15:55:33 +0800

    Add day 14 part 2

--- 2019/python/day14.py
+++ 2019/python/day14.py
@@ -21,11 +21,13 @@ for line in lines:
     'quantity': int(pcount),
     'ingredients': ilist
   }
-def solve():
+
+def solve(num_fuel):
   surplus = { }
   total_ore = 0
   deque = []
-  deque.extend(reactions['FUEL']['ingredients'])
+  starting_ingredients = [(key[0], key[1]*num_fuel) for key in reactions['FUEL']['ingredients']]
+  deque.extend(starting_ingredients)
   while len(deque) != 0:
     chemical, quantity_needed = deque.pop(0)
     if chemical == 'ORE':
@@ -41,5 +43,28 @@ def solve():
       for ingredient in ingredients:
         ingredient_name, ingredient_quantity = ingredient
         deque.append((ingredient_name, multiple * ingredient_quantity))
-  # print('ore needed', total_ore)
-  return total_ore, surplus
+  return total_ore
+
+
+def solve2():
+  ore_per_fuel = solve(1)
+  print('ore per fuel', ore_per_fuel)
+  total_ore = 1000000000000
+  total_fuel = math.floor(total_ore / ore_per_fuel) 
+  mintry = total_fuel
+  maxtry = total_fuel * 2
+  guess = mintry
+
+  while maxtry - mintry > 1:
+    guess = (maxtry + mintry) // 2
+    ore_needed = solve(guess)
+    if ore_needed > total_ore:
+      maxtry = guess
+    else:
+      mintry = guess
+  check = solve(guess)
+  if check > total_ore:
+    return guess - 1
+  return guess
+
+print('fuel produced', solve2())
